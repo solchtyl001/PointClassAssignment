a9857b0f34c3521ae9d1f3963426f03ed018f751
solchtyl001 <88106057+solchtyl001@users.noreply.github.com>
2022-02-16 17:12:36 -0500

    Update Main.java

--- src/org/WalkerCC_CompSci/Main.java
+++ src/org/WalkerCC_CompSci/Main.java
@@ -31,4 +31,38 @@ public class Main {
 
 
     }
+    
+    public class Point {
+    private double x = 0;
+    private double y = 0;
+
+    public Point(double x, double y){
+     this.x = x;
+     this.y = y;
+
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y ;
+    }
+
+    public void FlipVertical() {
+
+        this.x *= 1;
+    }
+
+    public void FlipHorizontal() {
+        this.x *= -1;
+    }
+
+    public void Translate(double xChange, double yChange){
+        this.x += xChange;
+                this.y += yChange;
+
+    }
+}
 }
